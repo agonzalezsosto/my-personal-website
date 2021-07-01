9344f8aaf9e89097d07c015fb22583664f969aee
Armando Gonzalez <agonzalezsosto@gmail.com>
2021-07-01 17:05:22 +0100

    end of session
    This is how the repository was at the end of the session. Next week we'll continue from here.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,53 @@
-import { Box } from '@material-ui/core'
+import { Box, makeStyles } from '@material-ui/core'
+
+const tabNames = [
+  'armando gonzalez',
+  'portfolio',
+  'contact',
+  'bio',
+  'my dogs bio',
+]
+
+const useStyles = makeStyles({
+  container: {
+    display: 'flex',
+    flexDirection: 'row',
+    position: 'fixed',
+    top: 0,
+    left: 0,
+    right: 0,
+    height: 100,
+  },
+  headerElement: {
+    marginLeft: 20,
+    fontSize: 30,
+    display: 'flex',
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    cursor: 'pointer',
+  },
+})
 
 function App() {
-  return <Box>Helloooo</Box>
+  const classes = useStyles()
+
+  return (
+    <Box className={classes.container}>
+      {tabNames.map((item) => {
+        return (
+          <Box
+            className={classes.headerElement}
+            onClick={() => {
+              console.log(item)
+            }}
+          >
+            {item}
+          </Box>
+        )
+      })}
+    </Box>
+  )
 }
 
 export default App
